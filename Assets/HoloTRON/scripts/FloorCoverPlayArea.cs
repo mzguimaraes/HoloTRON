c7c29ba3022da85a7c0653fedf04b5f7abe4b6bd
Marcus Guimaraes <mzg218@nyu.edu>
2017-04-14 18:45:06 -0400

    floor resizes to size of calibrated play area

---
+++ Assets/HoloTRON/scripts/FloorCoverPlayArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCoverPlayArea : MonoBehaviour {
+
+    public SteamVR_PlayArea playArea;
+    public float floorThickness = .25f;
+
+    Valve.VR.HmdQuad_t rect;
+
+	// Use this for initialization
+	void Start () {
+        rect = new Valve.VR.HmdQuad_t();
+        SteamVR_PlayArea.GetBounds(SteamVR_PlayArea.Size.Calibrated, ref rect);
+
+        transform.localScale = new Vector3(Mathf.Abs(rect.vCorners0.v0 - rect.vCorners2.v0), floorThickness, Mathf.Abs(rect.vCorners0.v2 - rect.vCorners2.v2));
+        transform.position = new Vector3(0f, -floorThickness / 2f, 0f);
+	}
+}
