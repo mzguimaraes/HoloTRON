cf41739bd18c0bac1e5f10ac759372acc60967a3
Marcus Guimaraes <mzg218@nyu.edu>
2017-04-14 17:54:52 -0400

    added cooldown to wall gun

--- Assets/HoloTRON/scripts/WallGun.cs
+++ Assets/HoloTRON/scripts/WallGun.cs
@@ -7,16 +7,24 @@ using FRL.IO;
 public class WallGun : MonoBehaviour, IGlobalTriggerPressDownHandler {
 
     public GameObject wallPrefab;
+    public float cooldown = 3f;
+    private float cdTimer = 0f;
+
+    void Update ()
+    {
+        if (cdTimer > 0f)
+        {
+            cdTimer -= Time.deltaTime;
+        }
+    }
 
     public void OnGlobalTriggerPressDown(VREventData eventData)
     {
-
-        Vector3 goal = eventData.module.transform.forward;
-        goal = Vector3.ProjectOnPlane(goal, Vector3.up);
-
-        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, goal);
-
-        Instantiate(wallPrefab, eventData.module.transform.position, eventData.module.transform.rotation);
+        if (cdTimer <= 0f)
+        {
+            cdTimer = cooldown;
+            Instantiate(wallPrefab, eventData.module.transform.position, eventData.module.transform.rotation);
+        }
     }
     
 }
