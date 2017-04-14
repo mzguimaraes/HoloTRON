693bad8e85f48d248c7ad98ea09a062ad314ff2c
Marcus Guimaraes <mzg218@nyu.edu>
2017-04-14 19:47:25 -0400

    haptic feedback

--- Assets/HoloTRON/scripts/WallGun.cs
+++ Assets/HoloTRON/scripts/WallGun.cs
@@ -18,19 +18,23 @@ public class WallGun : MonoBehaviour, IGlobalTriggerPressDownHandler {
         }
     }
 
-    void FireWall(Transform currTransform)
+    void FireWall(VREventData eventData)
     {
         //TODO: add haptic + visual feedback
-        Instantiate(wallPrefab, currTransform.position, currTransform.rotation);
+        ViveControllerModule vive = (ViveControllerModule)eventData.module;
+        vive.TriggerHapticPulse(.3f, 3999);
+
+        Instantiate(wallPrefab, eventData.module.transform.position, eventData.module.transform.rotation);
         
     }
 
     public void OnGlobalTriggerPressDown(VREventData eventData)
     {
+        
         if (cdTimer <= 0f)
         {
             cdTimer = cooldown;
-            FireWall(eventData.module.transform);
+            FireWall(eventData);
         }
     }
     
